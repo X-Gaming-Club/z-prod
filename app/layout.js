de5fbfa833695477e61ddab61cb2aa7adf89033d
singhalaryann <aryan.singhal.20cse@bmu.edu.in>
2025-04-05 03:18:37 +0530

    fixed sidebar

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,20 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Insights at Scale',
-  description: 'A Claude-like UI clone built with Next.js',
+  title: 'XGaming',
+  description: 'Insights at Scale',
+  icons: {
+    apple: [
+      { url: "/apple-touch-icon.png" },
+    ],
+    shortcut: ["/favicon-16x16.png"],
+    icon: [
+      { url: "/favicon.ico", sizes: "16x16" },
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+      { url: "/android-chrome-192x192.png", sizes: "192x192" },
+      { url: "/android-chrome-512x512.png", sizes: "512x512" },
+    ],
+  }
 }
 
 export default function RootLayout({ children }) {
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    // Disable the development overlay/icon
-    devIndicators: {
-      buildActivity: false,
-      buildActivityPosition: 'bottom-right',
-    },
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  // Disable the development overlay/icon
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
+};
+
+export default nextConfig;
